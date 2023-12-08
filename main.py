13f604dd3a2700fa6aa499543c9ca0a27e112456
YourBestDream <ferari100w3@gmail.com>
2023-12-08 18:48:14 +0200

    Changed the deprecated format

--- main.py
+++ main.py
@@ -3,6 +3,6 @@ import ssl
 #from this file everything should be started
 
 if __name__ == '__main__':
-    context = ssl.SSLContext(ssl.PROTOCOL_TLS)
+    context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
     context.load_cert_chain('neptun.pem', 'neptun-key.pem')
     app.run(host="0.0.0.0",port="5000",debug=True)
